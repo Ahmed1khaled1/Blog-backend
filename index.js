705a97fab7a704688b342c0a0370984b37c2d97b
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2024-11-22 22:51:25 +0200

    1

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const { PORT, URI } = require("./config");
 const blogRoute = require('./routes/blogRoute')
 const cors = require('cors')
+const Blog = require("../models/blogModel");
 
 const app = express();
+
+app.use(express.json());
 
 app.use(
   cors({
@@ -16,13 +19,99 @@ app.use(
 );
 app.use(express.json());
 
-app.use("/blogs", blogRoute);
 
 mongoose
   .connect(URI)
   .then((conn) => {
     console.log(`Database Connected: ${conn.connection.host}`);
 
+  app.post("/blogs", async (req, res) => {
+    try {
+      if (!req.body) {
+        return res.status(400).json({ message: "all fields are required" });
+      }
+      const blog = await Blog.create(req.body);
+      return res.status(200).json(blog);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  // Route for Get All Blogs from database
+  app.get("/blogs", async (req, res) => {
+    try {
+      const blogs = await Blog.find({});
+
+      return res.status(200).json({
+        count: blogs.length,
+        data: blogs,
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ message: error.message });
+    }
+  });
+
+  // Route for Get One Blog from database by id
+  app.get("/blogs/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const blog = await Blog.findById(id);
+
+      return res.status(200).json(blog);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ message: error.message });
+    }
+  });
+
+  // Route for Update a Blog
+  app.put("/blogs/:id", async (request, response) => {
+    try {
+      if (!request.body) {
+        return response.status(400).send({
+          message: "Send all required fields: title, author, publishYear",
+        });
+      }
+
+      const { id } = request.params;
+
+      const result = await Blog.findByIdAndUpdate(id, request.body);
+
+      if (!result) {
+        return response.status(404).json({ message: "Blog not found" });
+      }
+
+      return response
+        .status(200)
+        .send({ message: "Blog updated successfully" });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+
+  // Route for Delete a blog
+  app.delete("/blogs/:id", async (request, response) => {
+    try {
+      const { id } = request.params;
+
+      const result = await Blog.findByIdAndDelete(id);
+
+      if (!result) {
+        return response.status(404).json({ message: "Blog not found" });
+      }
+
+      return response
+        .status(200)
+        .send({ message: "Blog deleted successfully" });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
   
 
     app.listen(PORT, () => {
